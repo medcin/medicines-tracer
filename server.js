e3c074822b0d5b3fdda593d949668bdb0b65f05d
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2023-05-15 12:20:51 +0300

    test as well

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const e_app = express();
 const path = require("path");
 const router = express.Router();
-e_app.listen(8080, ()=>{console.log("hi");})
+e_app.listen(5500, ()=>{console.log("hi");})
 e_app.use(express.static("public"));
 
 
@@ -10,9 +10,17 @@ e_app.use(express.static("public"));
 
 
 
-
+router.get("/index", (req,res)=>{
+    res.sendFile(path.join(__dirname,"public/front-end/index.html"))
+})
 router.get("/", (req,res)=>{
 res.sendFile(path.join(__dirname,"public/front-end/home.html"));
+});
+
+router.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/public/front-end/404.html"))
+
 })
+
 
 e_app.use("/", router);
