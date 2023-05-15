178147dae2232142890ef3f3fa94f0cd565ef794
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2023-05-15 11:19:11 +0300

    change dirctry

--- back-end/index.js
+++
@@ -1,8 +0,0 @@
- // Get the client number input field
- var clientNumberInput = document.getElementById('client-number');
-  
- // Add an event listener to the input field to save the value to localStorage
- clientNumberInput.addEventListener('input', function() {
-   localStorage.setItem('clientNumber', clientNumberInput.value);
- });
- console.log(clientNumberInput);
