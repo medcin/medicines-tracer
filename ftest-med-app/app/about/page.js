23a994c21204a05b415e82af0b35c37cc6b99cb7
Faisal00000111 <faiselalzahrani010@gmail.com>
2023-05-22 19:04:16 +0300

    changed about page

--- ftest-med-app/app/about/page.js
+++ ftest-med-app/app/about/page.js
@@ -4,8 +4,8 @@ import NextLink from 'next/link';
 export default function About() {
     return (
       <div>
-        <h1>About Me</h1>
-        <p>My name is John Doe and I'm a web developer.</p>
+        <h1>About Medicine tracker</h1>
+        <p>This application will track your medicines</p>
         <NextLink href="/">
           Home
         </NextLink>
