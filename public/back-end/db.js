b971fa912943fa623e79993e13fc388a2b83868e
Anas Alzahrani <anas1zhr@gmail.com>
2023-05-29 21:18:42 +0300

    made the db connection

--- public/back-end/db.js
+++ public/back-end/db.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB;
+const host = process.env.DB_HOST;
+
 const Pool = require("pg").Pool
 const pool = new Pool({
-    user: "",
-    password: "",
-    database: "",
-    host: "localhost",
+    user: username,
+    password: password,
+    database: database,
+    host: host,
     port: 5432
 });
 
