f80dc019ce145907f5c6c61762305ff7ebb3000a
Anas Alzahrani <anas1zhr@gmail.com>
2023-05-29 22:22:10 +0300

    testing if the env info are reachable

--- public/back-end/db.js
+++ public/back-end/db.js
@@ -4,12 +4,14 @@ const password = process.env.DB_PASSWORD;
 const database = process.env.DB;
 const host = process.env.DB_HOST;
 
+console.log(password);
+
 const Pool = require("pg").Pool
 const pool = new Pool({
-    user: username,
-    password: password,
-    database: database,
-    host: host,
+    user: this.username,
+    password: this.password,
+    database: this.database,
+    host: this.host,
     port: 5432
 });
 
