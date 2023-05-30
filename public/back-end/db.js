59625d11351411100a8b44092188ab92038dffe0
Faisal00000111 <faiselalzahrani010@gmail.com>
2023-05-30 16:09:16 +0300

    Fixed the DB problem, it's working fine

--- public/back-end/db.js
+++ public/back-end/db.js
@@ -4,15 +4,18 @@ const dbPassword = process.env.DB_PASSWORD;
 const dbDatabase = process.env.DB;
 const dbHost = process.env.DB_HOST;
 
-console.log(process.env.DB_PASSWORD, ' asd');
+console.log(process.env.DB_PASSWORD, " asd");
 
-const Pool = require("pg").Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: dbUsername,
-    password: dbPassword,
-    database: dbDatabase,
-    host: dbHost,
-    port: 5432
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  user: dbUsername,
+  password: dbPassword,
+  database: dbDatabase,
+  host: dbHost,
+  port: 5432
 });
 
 module.exports = pool;
