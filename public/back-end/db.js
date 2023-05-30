abe4912dfe5bf39aac6080ffd7fd0225fa6cd2e0
Faisal00000111 <faiselalzahrani010@gmail.com>
2023-05-30 15:40:53 +0300

    updated my branch to the latest version

---
+++ public/back-end/db.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const dbUsername = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbDatabase = process.env.DB;
+const dbHost = process.env.DB_HOST;
+
+console.log(process.env.DB_PASSWORD, ' asd');
+
+const Pool = require("pg").Pool
+const pool = new Pool({
+    user: dbUsername,
+    password: dbPassword,
+    database: dbDatabase,
+    host: dbHost,
+    port: 5432
+});
+
+module.exports = pool;
