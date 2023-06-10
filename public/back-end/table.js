9634a0178e51dd11d825df7fdd61b7236ca8a1b4
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2023-06-10 13:19:13 +0300

    this will make the table lloks good

--- public/back-end/table.js
+++ public/back-end/table.js
@@ -1,10 +1,109 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var Calendar = tui.Calendar; // Make sure tui.Calendar is available
+  const Calendar = tui.Calendar; // Make sure tui.Calendar is available
 
-  var calendar = new Calendar("#calendar", {
+  const calendar = new Calendar("#calendar", {
     defaultView: "week",
+    usageStatistics: false,
+    
 
+    template: {
+      monthDayname: function (dayname) {
+        return (
+          '<span class="calendar-week-dayname-name">' +
+          dayname.label +
+          "</span>"
+        );
+      },
+    },
+
+    disableDblClick: true, // Disable double-click to create a new event
+
+    disableClick: function (schedule) {
+      return schedule.category === "milestone"; // Disable click on milestone events
+    },
+
+    timezones: [
+      {
+        timezoneOffset: 9 * 60, // Set your desired timezone offset
+        displayLabel: "GMT+3:00", // Set the timezone display label
+        tooltip: "KSA", // Set the timezone tooltip
+      },
+    ],
+
+    useDetailPopup: false, // Disable the detail popup for events
+
+    isReadOnly: true, // Make the calendar read-only
+
+    disableAllday: true, // Disable the all-day option
+
+    disableSchedule: true, // Disable creating, modifying, and deleting events
+
+    disableDrag: true, // Disable dragging events
+
+    disableResize: true, // Disable resizing events
+    
+  });
+  
+  calendar.setOptions({ 
+    week: { 
+      taskView: false,
+      eventView: ["time"],
+      hourStart: 00,
+      hourEnd: 24,
+      showNowIndicator: false,
+
+
+
+    },
+
+  });
+
+  calendar.createEvents([
+    {
+      id: "1",
+      calendarId: "1",
+      title: "pandol",
+      category: "time",
+      dueDateClass: "",
+      start: "2023-06-10T22:30:00+3:00",
+      end: "2023-06-10T23:30:00+3:00",
+    },
+  ]);
+
+  let form = document.getElementById("appointmentform");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    console.log(title);
+    const start = document.getElementById("start").value;
+
+    const end = document.getElementById("end").value;
+
+    let appointment = {
+      id: String(Date.now()),
+      calendarId: "1",
+      title: title,
+      category: "time",
+      dueDateClass: "",
+      start: start,
+      end: end,
+    };
+
+    calendar.createEvents([appointment]);
+  });
+});
+
+/*document.addEventListener("DOMContentLoaded", function () {
+  const Calendar = tui.Calendar; // Make sure tui.Calendar is available
+
+  const calendar = new Calendar("#calendar", {
+    defaultView: "week",
     taskView: true,
+    usaageStatistics: false,
+
+
+
+    
 
     template: {
       monthDayname: function (dayname) {
@@ -47,9 +146,6 @@ form.addEventListener("submit", (e) => {
         end: end,
     }
 
-    
-
-
   calendar.createEvents([
     {   
         id: "1",
@@ -57,8 +153,8 @@ form.addEventListener("submit", (e) => {
         title: "pandol",
         category: "time",
         dueDateClass: "",
-        start: "2023-05-30T22:30:00+09:00",
-        end: "2023-05-30T23:30:00+09:00",
+        start: "2023-06-10T22:30:00+09:00",
+        end: "2023-06-10T23:30:00+09:00",
 
 
     }
