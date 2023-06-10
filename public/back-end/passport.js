4b6cf28d158689a2d525519d138e64e6e75acf1b
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2023-06-10 10:02:10 +0300

    sync

---
+++ public/back-end/passport.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+
+async function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = await getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: "Login information are incorrect!" });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.rows[0].password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, {
+          message: "Login information are incorrect!",
+        });
+      }
+    } catch (error) {
+      return done(error);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.rows[0].id);
+  });
+  passport.deserializeUser(async (id, done) => {
+    const user = await getUserById(id);
+    return done(null, user);
+  });
+}
+
+module.exports = initialize;
--- server.js
+++ server.js
@@ -1,79 +1,166 @@
 const express = require("express");
+const router = express.Router();
 const path = require("path");
+const passport = require("passport");
 const bodyParser = require("body-parser");
-const router = require("./public/back-end/routes/routing.js");
 const pool = require("./public/back-end/db.js");
 const { log } = require("console");
+const bcrypt = require("bcrypt");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const initializePassport = require("./public/back-end/passport");
+initializePassport(
+  passport,
+
+  async (email) => {
+    const user = await pool.query(
+      "SELECT * from public.users WHERE email= $1",
+      [email]
+    );
+    if (user.rowCount !== 0) {
+      return user;
+    } else {
+      return null;
+    }
+  },
+
+  async (id) => {
+    const user = await pool.query("SELECT * from public.users WHERE id= $1", [
+      id,
+    ]);
+    if (user.rowCount !== 0) {
+      return user;
+    } else {
+      return null;
+    }
+  }
+);
 
 const app = express();
 
+//------------------------------------------------------------------------------------------
 // app uses
-app.use(express.static("public")); // insteed of sending every file alone this would help u sending one file
-app.use(bodyParser()); // this allow u to manage reqs
-app.use("/", router); // router configeration you need it to use router.get but app.get u don't need to do that
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(bodyParser());
+app.use(router);
+app.use(cookieParser());
 
+//------------------------------------------------------------------------------------------
 // get pages
+router.get("/", checkNotAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/welcome.html"));
+});
+//===================
+router.get("/signup", checkNotAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/signup.html"));
+});
+//===================
+router.get("/login", checkNotAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/login.html"));
+});
+//===================
+router.get("/main", checkAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/main.html"));
+});
+//===================
+router.get("/my-meds", checkAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/medsTable.html"));
+});
+//===================
+router.get("/health-news", checkAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/health-news.html"));
+});
+//===================
+router.get("/settings", checkAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/settings.html"));
+});
+//===================
+router.get("/otp", checkNotAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/otp.html"));
+});
+//===================
+// the * means any miss spiled rout will lead to 404 page
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/front-end/html/404.html"));
+});
+//===================
 
+//------------------------------------------------------------------------------------------
 // post pages
-
+// Signup
 router.post("/otp", async (req, res) => {
   try {
-    // console.log(req.body);
-    /* User registeration info are fetched successfully
-  We just need to send it to the database*/
     const { fullName, userName, email, number, password } = req.body;
-    // console.log(fullName, userName, email, number, password);
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await pool.query(
       "INSERT INTO public.users (name, phone, email, password, username) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [fullName, number, email, password, userName]
+      [fullName, number, email, hashedPassword, userName]
     );
-    res.sendFile(path.join(__dirname, "/public/front-end/html/otp.html"));
-  } catch (error) {
-    console.error(error.message);
+    res.redirect("/otp");
+  } catch {
+    res.redirect("/signup");
   }
 });
 
+//------------------------------------------------------------------------------------------
+// OTP
 router.post("/login", (req, res) => {
   try {
     const { otp } = req.body;
-    console.log(req.body);
-    /* User registeration info are fetched successfully
-    We just need to send it to the database*/
-
-    res.sendFile(path.join(__dirname, "/public/front-end/html/login.html"));
+    res.redirect("/login");
   } catch (error) {
     console.error(error.message);
   }
 });
 
-router.post("/main", async (req, res) => {
-  try {
-    console.log(req.body);
+//------------------------------------------------------------------------------------------
+// Login
+router.post(
+  "/main",
+  passport.authenticate("local", {
+    successRedirect: "/main",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-    /* User login info are fetched successfully
-    We just need to verify it*/
-    const { email, password } = req.body;
-    // console.log("SELECT * from public.users WHERE email= $1 , password= $2 ", [
-    //   email,
-    //   password,
-    // ]);
-
-    const user = await pool.query(
-      "SELECT * from public.users WHERE email= $1 AND password= $2",
-      [email, password]
-    );
-
-    console.log(user.rows);
-    if (user.rows.length > 0) {
-      res.sendFile(path.join(__dirname, "/public/front-end/html/main.html"));
-    } else {
-      res.send("The email or password is wrong");
-    }
-  } catch (error) {
-    res.send(error.message);
-  }
+//------------------------------------------------------------------------------------------
+// Log out
+router.delete("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
 });
 
-app.listen(5500, () => {
+//------------------------------------------------------------------------------------------
+// Functions
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/main");
+  }
+  next();
+}
+//------------------------------------------------------------------------------------------
+app.listen(5500, async () => {
   console.log("hi");
 });
